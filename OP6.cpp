104aee5d251ebb2727ca3997a51c4fc74c2f28ca
F1xEat <93028837+F1xEat@users.noreply.github.com>
2021-11-05 09:48:49 +0200

    Add files via upload

---
+++ OP6.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream> 
+#include <iomanip> 
+#include "windows.h"
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cout << "Enter a = ";
+	cin >> a;
+	cout << "Enter b = ";
+	cin >> b;
+	cout << "Enter c = ";
+	cin >> c;
+	float x = a, y;
+	while (x <= b)
+	{
+		y = pow(x,8) + 5 * x*x - 5;
+		cout << "x=" << x << setw(5) << "y=" << y << endl;
+		x += c;
+		return 0;
+	}
+}
